6f47f44519d136f2b9e863607d544eec9b581564
jukgei <jukgei@outlook.com>
2023-09-26 23:36:47 +0800

    Dimensionality-independent support 2D/3D

--- main.py
+++ main.py
@@ -11,9 +11,11 @@ dim = 3
 if dim == 2:
     vec = ti.math.vec2
     mat = ti.math.mat2
+    index = ti.math.ivec3
 else: # 3d
     vec = ti.math.vec3
     mat = ti.math.mat3
+    index = ti.math.ivec4
 
 width = 640
 height = 640
@@ -24,24 +26,26 @@ if dim == 2:
     center = ti.Vector([0.72, 0.8])
     g_dir = [0, -1]
     block_radius = 0.33
+    delta_time = 5e-5
+    damping = 14.5
+    rho = 500
 else:
     block_center = [0.5, 0.5, 0.5]
     center = ti.Vector([2, 1, 2])
     g_dir = [0, -1, 0]
     block_radius = 0.0
+    delta_time = 5e-4
+    damping = 5
+    rho = 1000
 
 E, nu = 4e4, 0.2  # Young's modulus and Poisson's ratio
 mu, s_lambda = E / 2 / (1 + nu), E * nu / (1 + nu) / (1 - 2 * nu)
-damping = 5
 
 # center = ti.Vector([0.55, 0.3])
 v_refect = -0.3
-delta_time = 5e-4
+# delta_time = 5e-4
 side_length = 0.2  #
 subdivisions = 10  #
-
-rho = 1000
-
 
 auto_diff = True
 
@@ -63,11 +67,11 @@ if dim == 2:
     element_indices = faces
     A = ((side_length / subdivisions) ** 2) / 2
     mass = rho * A
+    num_sides = 3
 else:
     # mm = tm.load_mesh('./obj/stanford-bunny.obj')
     obj = tm.load_mesh('./obj/cube2.stl')
     obj.apply_scale(1)
-    # mesh = tm.Trimesh(vertices=mm.vertices, faces=mm.faces)
     msh = gmsh.to_volume(obj, './obj/cube2.msh', mesher_id=7)
     mesh4 = mio.read('./obj/cube2.msh')
     vertices = mesh4.points
@@ -75,21 +79,6 @@ else:
     # 获取四面体的点的indices
     tetra_indices = mesh4.cells[0].data
     element_indices = tetra_indices
-    # 遍历每个四面体
-    for tetra in tetra_indices:
-        # 获取四面体四个点的索引
-        point_indices = tetra
-
-        # 获取四面体四个点的坐标
-        tetra_points = vertices[point_indices]
-
-        # 打印四面体的信息
-        print("Tetrahedron:")
-        print("Point Indices:", point_indices)
-        print("Points:")
-        for point in tetra_points:
-            print(point)
-        print()
 
     faces = []
     for tetra in tetra_indices:
@@ -102,6 +91,8 @@ else:
     # faces = tetra_indices # Volume
     # vertices = mesh.vertices
     mass = rho / tetra_indices.shape[0]
+    num_sides = 4
+
     # Boundary Box
     box_min = ti.Vector([0, 0, 0])
     box_max = ti.Vector([5, 5, 5])
@@ -122,7 +113,7 @@ ti_vertices = ti.Vector.field(dim, ti.f32, shape=vertices.shape[0])
 ti_vertices.from_numpy(vertices)
 ti_faces = ti.Vector.field(3, ti.i32, shape=faces.shape[0])
 ti_faces.from_numpy(faces)
-ti_element = ti.Vector.field(4, ti.i32, shape=element_indices.shape[0])
+ti_element = ti.Vector.field(num_sides, ti.i32, shape=element_indices.shape[0])
 ti_element.from_numpy(element_indices)
 
 phi = ti.field(dtype=ti.f32, shape=faces.shape[0])
@@ -146,10 +137,7 @@ Mesh = ti.types.struct(
 )
 
 Element = ti.types.struct(
-    p0=ti.i32,
-    p1=ti.i32,
-    p2=ti.i32,
-    p3=ti.i32,
+    vertex_indices=index,
     ref=mat
 )
 
@@ -159,37 +147,45 @@ element_cnt = element_indices.shape[0]
 meshs = Mesh.field(shape=mesh_cnt)
 elements = Element.field(shape=element_cnt)
 particles = Particle.field(shape=particle_cnt, needs_grad=True)
+
 print('Vertex count: {}'.format(particle_cnt))
 print('Mesh count: {}'.format(mesh_cnt))
 print('Element count: {}'.format(element_cnt))
 print('Element mass: {}'.format(mass))
 
+@ti.func
+def compute_volume(x: mat) -> ti.f32:
+    m = ti.math.mat3(0)
+    V = 0.0
+    for i in ti.static(range(x.m)):
+        for j in ti.static(range(x.n)):
+            m[i, j] = x[i, j]
+
+    p0 = m[:, 0]
+    p1 = m[:, 1]
+    p2 = m[:, 2]
+
+    if dim == 2:
+        V = ti.abs(p0.cross(p1).norm()) / 2
+    else:
+        V = (1/6) * ti.abs(p0.dot(p1.cross(p2)))
+    return V
+
+
 @ti.kernel
 def compute_energy():
     for i in range(element_cnt):
         element = elements[i]
-        p0 = particles[element.p0].pos
-        p1 = particles[element.p1].pos
-        p2 = particles[element.p2].pos
-        p3 = particles[element.p3].pos
-        p10 = p1 - p0
-        p20 = p2 - p0
-        p30 = p3 - p0
-        # I = mat([1, 0, 0, 1])
-        I = mat([1, 0, 0, 0, 1, 0, 0, 0, 1])
-        # X = mat([p10.x, p20.x, p10.y, p20.y])
-        X = ti.Matrix.cols([p10, p20, p30])
+        p_0 = particles[element.vertex_indices.x].pos
+        X = mat(0)
+        for j in ti.static(range(dim)):
+            if j + 1 <= dim:
+                p_j = particles.pos[element.vertex_indices[j+1]]
+                X[:, j] = p_j - p_0
 
         R_inv = element.ref
         F = X @ R_inv
-        # if i == 1:
-        #     print(X)
-        #     print('F {}'.format(X @ ti.math.inverse(X)))
-        # S = ti.abs(p10.cross(p20))
-        # S = p10.cross(p20).norm() / 2
-        V = (1/6) * ti.abs(p10.dot(p20.cross(p30)))
-        # K = G.transpose() @ G
-        # U[None] += S * (0.5 * s_lambda * G.trace() **2 + mu * K.trace())
+        V = compute_volume(X)
 
         # Neo-Hookean
         F_i = F
@@ -198,17 +194,15 @@ def compute_energy():
         phi_i -= mu * log_J_i
         phi_i += s_lambda / 2 * log_J_i ** 2
         phi[i] = phi_i * V
-        # print(phi[i], log_J_i, F_i.determinant())
-        # print(U[None], S * phi_i)
         U[None] += V * phi_i
 
-
         # StVK
+        # I = ti.math.eye(dim)
         # G = 0.5 * (F.transpose() @ F - I)
         # phi_i = (G ** 2).sum() * mu
         # phi_i += s_lambda/2 * G.trace() **2
-        # phi[i] = phi_i * S
-        # U[None] += S * phi_i
+        # phi[i] = phi_i * V
+        # U[None] += V * phi_i
 
 
 @ti.kernel
@@ -228,32 +222,18 @@ def kinematic(index: ti.int32, dt: ti.f32):
 
     particles[index].vel *= ti.exp(-dt * damping)
 
-    if particles[index].pos.x > 1 and particles[index].vel.x > 0:
-        particles[index].vel.x = 0
+    for i in ti.static(range(dim)):
+        if particles.pos[index][i] < 0 and particles.vel[index][i] < 0:
+            particles.vel[index][i] = 0
 
-    if particles[index].pos.y > 1 and particles[index].vel.y > 0:
-        particles[index].vel.y = 0
-
-    if particles[index].pos.z > 1 and particles[index].vel.z > 0:
-        particles[index].vel.z = 0
-
-    if particles[index].pos.x < 0 and particles[index].vel.x < 0:
-        particles[index].vel.x = 0
-
-    if particles[index].pos.y < 0 and particles[index].vel.y < 0:
-        particles[index].vel.y = 0
-
-    if particles[index].pos.z < 1 and particles[index].vel.z < 0:
-        particles[index].vel.z = 0
+        if particles.pos[index][i] > 1 and particles.vel[index][i] > 0:
+            particles.vel[index][i] = 0
 
     if (particles[index].pos - ti.Vector(block_center)).norm() < block_radius and particles[index].vel.dot(ti.Vector(block_center) - particles[index].pos) > 0:
         disp = particles[index].pos - ti.Vector(block_center)
         particles[index].vel -= particles[index].vel.dot(disp) * disp / disp.norm_sqr()
-        # particles[index].vel =ti.Vector([-n.y, n.x]) * v_norm
 
     particles.pos[index] += particles.vel[index] * dt
-    # if index == 0:
-    #     print(particles.vel[index])
     if not auto_diff:
         particles[index].force = vec(0.0)
 
@@ -268,17 +248,14 @@ def particles_init():
 @ti.kernel
 def elements_init():
     for i in range(element_cnt):
-        elements[i].p0 = ti_element[i][0]
-        elements[i].p1 = ti_element[i][1]
-        elements[i].p2 = ti_element[i][2]
-        elements[i].p3 = ti_element[i][3]
+        elements[i].vertex_indices = ti_element[i]
 
-        a, b, c, d = ti_element[i]
-        p_a, p_b, p_c, p_d = particles[a].pos, particles[b].pos, particles[c].pos, particles[d].pos
-        # if dim == 2:
-        #     r = ti.Matrix.cols([p_b - p_a, p_c - p_a])
-        # else:
-        r = ti.Matrix.cols([p_b - p_a, p_c - p_a, p_d - p_a])
+        r = mat(0)
+        p_0 = particles[ti_element[i].x].pos
+        for j in ti.static(range(dim)):
+            if j + 1 <= dim:
+                p_i = particles[ti_element[i][j+1]].pos
+                r[:, j] = p_i - p_0
 
         elements[i].ref = ti.math.inverse(r)
 
@@ -350,7 +327,7 @@ def render3d(window, camera):
     camera.track_user_inputs(window, movement_speed=0.05, hold_key=ti.ui.RMB)
     scene.set_camera(camera)
     scene.ambient_light((0.8, 0.8, 0.8))
-    scene.point_light(pos=(0.5, 1.5, 1.5), color=(1, 1, 1))
+    scene.point_light(pos=(3.5, 3.5, 3.5), color=(1, 1, 1))
     scene.lines(box_vert, indices=box_lines_indices, color=(0.99, 0.68, 0.28), width=2.0)
     pos_ = particles.pos.to_numpy()
 
@@ -361,13 +338,12 @@ def render3d(window, camera):
 
 
 if __name__ == '__main__':
-
     if dim == 2:
         gui = ti.GUI('Finite Element Method', (width, height))
         widget = gui
         camera = None
     else:
-        window = ti.ui.Window('Window Title', res=(width, height), pos=(150, 150))
+        window = ti.ui.Window('Finite Element Method', res=(width, height), pos=(150, 150))
 
         canvas = window.get_canvas()
 
@@ -399,40 +375,12 @@ if __name__ == '__main__':
             if not auto_diff:
                 fem()
             else:
-                pass
                 # if frame_cnt == 1:
                 with ti.ad.Tape(loss=U):
                     compute_energy()
             kinematic_mesh()
 
-        # for i in range(particle_cnt):
-        #     begin = particles[i].pos
-        #     end = particles[(i+1)%particle_cnt].pos
-        #     gui.line(begin, end, radius=1, color=0xFF0000)
-        # for i in range(mesh_cnt):
-        #     p0 = particles[meshs[i].p0].pos
-        #     p1 = particles[meshs[i].p1].pos
-        #     p2 = particles[meshs[i].p2].pos
-        #     gui.line(p0, p1, radius=1, color=0xFF0000)
-        #     gui.line(p1, p2, radius=1, color=0xFF0000)
-        #     gui.line(p2, p0, radius=1, color=0xFF0000)
-
         if dim == 2:
             render2d(widget)
         else:
-            # canvas = window.get_canvas()
-            # scene = ti.ui.Scene()
-
-            # # Camera & light
-            # camera.track_user_inputs(window, movement_speed=0.05, hold_key=ti.ui.RMB)
-            # scene.set_camera(camera)
-            # scene.ambient_light((0.8, 0.8, 0.8))
-            # scene.point_light(pos=(0.5, 1.5, 1.5), color=(1, 1, 1))
-            #
-            # scene.particles(particles.pos, color=(1.0, 1.0, 1), radius=1)
-            # # print('1')
-            # canvas.scene(scene)
-            # window.show()
             render3d(widget, camera)
-            pass
-            # render3d(widget)
